687d72359bebb5f4391997e553772e0dad126b09
kaushikchoudhury <kaushikc44@gmail.com>
2023-11-08 22:14:23 +0530

    added timer ot the collapse

--- components/WalletExist.jsx
+++ components/WalletExist.jsx
@@ -1,61 +1,44 @@
-import Alert from '@mui/material/Alert';
-import Stack from '@mui/material/Stack';
-import Collapse from '@mui/material/Collapse';
-import { useState } from 'react';
-import IconButton from '@mui/material/IconButton';
-import Box from '@mui/material/Box';
-import CloseIcon from '@mui/icons-material/Close';
+import React, { useState, useEffect } from 'react';
 
+function MyComponent() {
+  const [open, setOpen] = useState(true);
 
-export default function WalletExist(){
-    const [open,setOpen] = useState(true)
-    let injectProvider =  false
-    if (typeof window.mina !== 'undefined') {
-        injectProvider = true
+  useEffect(() => {
+    if (open) {
+      const timer = setTimeout(() => {
+        setOpen(false);
+      }, 3000); // 3000 milliseconds (3 seconds)
+
+      return () => {
+        clearTimeout(timer);
+      };
     }
-    return (
-        <>
-            {injectProvider ? ( <Box sx={{ width: '100%' }}>
-                <Collapse in={open}>
-                    <Alert
-                    action={
-                        <IconButton
-                        aria-label="close"
-                        color="inherit"
-                        size="small"
-                        onClick={() => {
-                            setOpen(false);
-                        }}
-                        >
-                            <CloseIcon fontSize="inherit" />
-                        </IconButton>
-                    }
-                    sx={{ mb: 2 }}
-                    >
-                        Auro does exists
-                    </Alert>
-                </Collapse>
-            </Box>) : ( <Box sx={{ width: '100%' }}>
-                <Collapse in={open}>
-                    <Alert
-                    action={
-                        <IconButton
-                        aria-label="close"
-                        color="inherit"
-                        size="small"
-                        onClick={() => {
-                            setOpen(false);
-                        }}
-                        >
-                            <CloseIcon fontSize="inherit" />
-                        </IconButton>
-                    }
-                    sx={{ mb: 2 }} severity="error"
-                    >
-                        Auro does not exists!
-                    </Alert>
-                </Collapse>
-            </Box>)}
-        </>
-    )
+  }, [open]);
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Collapse in={open}>
+        <Alert
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          sx={{ mb: 2 }}
+          severity={injectProvider ? 'info' : 'error'}
+        >
+          {injectProvider ? 'Auro does exist' : 'Auro does not exist!'}
+        </Alert>
+      </Collapse>
+    </Box>
+  );
 }
+
+export default MyComponent;
